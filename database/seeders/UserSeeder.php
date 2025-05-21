ea82097c38cba47fb6e6bd14c996c4ae16400d96
Jason <jason.535220045@stu.untar.ac.id>
2025-05-21 22:24:31 +0700

    Updated UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,11 +11,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Seed an admin user
+        User::create([
+            'user_id' => '00000',
+            'username' => 'Admin',
+            'email' => 'admin@adopt.com',
+            'password' => bcrypt('Admin123'), 
+            'phone' => '0812345678901',
+            'picture' => 'images/default.png',
+            'terms' => true,
+            'is_admin' => true, 
+        ]);
+
         User::factory()
         ->withReference()
         ->count(10)
         ->create();
-
 
     }
 }
